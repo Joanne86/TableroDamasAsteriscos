aeb2594609684f3856726a572d6d7e820239a813
LEIDY RODRIGUEZ <LEIDY RODRIGUEZ@leidy-PC>
2018-03-16 12:52:16 -0500

    Resuelto

---
+++ src/tablerodamasasteriscos/TableroDamasAsteriscos.java
@@ -0,0 +1,20 @@
+package tablerodamasasteriscos;
+
+public class TableroDamasAsteriscos {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 8; i++) {
+
+            for (int j = 0; j < 8; j++) {
+                
+                if(i%2==0){
+                   System.out.print("* "); 
+                }else{
+                    System.out.print(" *"); 
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
